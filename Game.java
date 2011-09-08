01e20b10cb1f15232b63402522139e0e3180cba7
Mike Guay <MikeRGuay@gmail.com>
2011-09-08 15:02:56 -0400

    Added Game.java

---
+++ Game.java
@@ -0,0 +1,44 @@
+/*
+ * Game.java
+ *
+ * Version:
+ *    $Id$
+ *
+ * Revisions:
+ *    &Log$
+ *
+ */
+
+/** 
+ * This class specifies the game state for a 2-dimensional board game.
+ *
+ * @author     YOUR NAME GOES HERE
+ *
+ */
+
+public class Game {
+    
+    final static int BLACK = 1;          // Declare state of each square
+    final static int WHITE = 2;
+    final static int EMPTY = 0;
+    final static int WIDTH = 10;
+    final static int HEIGHT = 10;
+    final static int board[][] = new int[WIDTH][HEIGHT];
+
+    /**
+     *  Decide if the move is legal
+     *
+     *  @param    r          row in the game matrix
+     *  @param    c          column in the game matrix
+     *  @param    color      color of the player - Black or White
+     *  @param    flip       true if the player wants to flip the discs
+     *
+     *  @return              true if the move is legal, else false
+     */
+    public boolean legalMove(int r, int c, int color, boolean flip) {
+       
+        boolean legal = true;
+
+        return legal;
+    }
+}
