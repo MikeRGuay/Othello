0b8f3100d6cdadbfbd93d2983edf486f4d5c0f27
Mike Guay <MikeRGuay@gmail.com>
2011-09-08 17:07:04 -0400

    Added code to legal moves.

--- Game.java
+++ Game.java
@@ -36,9 +36,14 @@ public class Game {
      *  @return              true if the move is legal, else false
      */
     public boolean legalMove(int r, int c, int color, boolean flip) {
-       
         boolean legal = true;
-
+	if( board[r][c] != 0 ) legal = false; //check if square is empty
+	if( r == 0 || c == 0 || 
+		r == 10 || c == 10) legal = false; // check if square is within
+						   // legal bounds.
+	
+	
+	
         return legal;
     }
 }
